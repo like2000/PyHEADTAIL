07c834de3c67f5ff341126ce9ff88da345ca4525
Alexandre Lasheen <alexandre.lasheen@cern.ch>
2014-06-20 13:57:29 +0200

    Changed structure of RF_section_parameters, one can sum RF_section_parameters objects to generate the RF parameters for a complete ring (or sum of sections)

--- input_parameters/rf_parameters.py
+++ input_parameters/rf_parameters.py
@@ -7,6 +7,8 @@ Module gathering and processing all the RF parameters to be given to the other m
 '''
 
 import numpy as np
+import copy
+
 
 def input_check(input_value, expected_length):
     '''
@@ -17,7 +19,7 @@ def input_check(input_value, expected_length):
     
     if type(input_value) is float:
         return input_value * np.ones(expected_length)
-    if type(input_value) is int:
+    elif type(input_value) is int:
         return input_value * np.ones(expected_length)
     elif type(input_value) is np.ndarray and input_value.size == 1:
         return input_value * np.ones(expected_length)
@@ -34,71 +36,76 @@ class RF_section_parameters(object):
     a complete set of parameters to be used afterwards
     '''
     
-    def __init__(self, n_rf_systems, harmonic_numbers_list, voltage_program_list, phi_offset_list, momentum_program):
+    def __init__(self, n_turns, n_rf_systems, section_length, harmonic_numbers_list, voltage_program_list, phi_offset_list, momentum_program):
+        
+        self.n_sections = 1 #: 1 section at initialisation
+        self.n_turns = n_turns
+        self.section_length = section_length #: Length of the section in [m]
+        self.n_rf_systems = n_rf_systems #: Number of RF systems in the section
+        self.momentum_program = momentum_program #: Momentum program in [eV/c]
+        
+        self.momentum_program = input_check(momentum_program, self.n_turns + 1)
         
         if n_rf_systems == 1:
-            self.n_rf_systems = n_rf_systems #: Number of RF systems in the section
             self.harmonic_numbers_list = [harmonic_numbers_list] #: Harmonic number list (according to n_rf_systems)
             self.voltage_program_list = [voltage_program_list] #: Voltage program list in [V] (according to n_rf_systems)
             self.phi_offset_list = [phi_offset_list] #: Phase offset list in [rad]
-            self.momentum_program = momentum_program #: Momentum program in [eV/c]
-            
         else:
             if not n_rf_systems == len(harmonic_numbers_list) == len(voltage_program_list) == len(phi_offset_list):
                 raise RuntimeError('The RF parameters to define RF_section_parameters are not homogeneous')
-        
-            self.n_rf_systems = n_rf_systems #: Number of RF systems in the section
             self.harmonic_numbers_list = harmonic_numbers_list #: Harmonic number list (according to n_rf_systems)
             self.voltage_program_list = voltage_program_list #: Voltage program list in [V] (according to n_rf_systems)
             self.phi_offset_list = phi_offset_list #: Phase offset list in [rad]
-            self.momentum_program = momentum_program #: Momentum program in [eV/c]
+            
+        for i in range(self.n_rf_systems):
+            self.harmonic_numbers_list[i] = input_check(self.harmonic_numbers_list[i], n_turns)
+            self.voltage_program_list[i] = input_check(self.voltage_program_list[i], n_turns)
+            self.phi_offset_list[i] = input_check(self.phi_offset_list[i], n_turns)
+            
+    def __add__(self, Added_RF_section_parameters):
+        '''
+        Method to add one section to another
+        '''
         
+        new_RF_section_parameters = copy.deepcopy(self)
+        
+        assert new_RF_section_parameters.n_turns == Added_RF_section_parameters.n_turns
+        
+        new_RF_section_parameters.section_length += Added_RF_section_parameters.section_length
 
-class RF_parameters(object):
-    '''
-    Object containing all the RF parameters and generating arrays to be used
-    in the tracking
-    '''
+        new_number_of_sections = new_RF_section_parameters.n_sections +  Added_RF_section_parameters.n_sections
+        
+        temp_momentum = np.zeros((new_number_of_sections, new_RF_section_parameters.n_turns + 1))
+        temp_momentum[range(new_RF_section_parameters.n_sections),:] = new_RF_section_parameters.momentum_program
+        temp_momentum[range(new_number_of_sections-Added_RF_section_parameters.n_sections, new_number_of_sections),:] = Added_RF_section_parameters.momentum_program
 
-    def __init__(self, n_turns, n_sections, RF_section_parameters_list):
+        new_RF_section_parameters.n_sections = new_number_of_sections
+        new_RF_section_parameters.momentum_program = temp_momentum
+        
+        temp_n_rf_systems = []
+        temp_n_rf_systems.append(new_RF_section_parameters.n_rf_systems)
+        temp_n_rf_systems.append(Added_RF_section_parameters.n_rf_systems)
+        
+        new_RF_section_parameters.n_rf_systems = temp_n_rf_systems
 
-        self.n_sections = n_sections #: Number of sections of the ring
-        self.n_turns = n_turns #: Number of turns for the simulation
+        temp_harmonic_numbers_list = []
+        temp_harmonic_numbers_list.append(new_RF_section_parameters.harmonic_numbers_list)
+        temp_harmonic_numbers_list.append(Added_RF_section_parameters.harmonic_numbers_list)
+        new_RF_section_parameters.harmonic_numbers_list = temp_harmonic_numbers_list
         
-        # Checking the validity of the input and warning
-        if len(RF_section_parameters_list) != n_sections:
-            raise RuntimeError(str(RF_section_parameters_list) + ' does not match with the number of sections')
-        
-        self.momentum_program = np.zeros((self.n_sections, n_turns + 1)) #: Reorganizing the momentum_program as a matrix
-        rf_input_section_arr = [] #: Reorganizing the rf_input as a list of np.arrays, for one section
-        self.rf_input_total_arr = [] #: Gathering all the np.arrays of all the sections in one list
-        
-        for i in range(n_sections):
-            self.momentum_program[i,:] = input_check(RF_section_parameters_list[i].momentum_program, n_turns + 1)
-                
-            for j in range(RF_section_parameters_list[i].n_rf_systems):
-                harmonic_number_array = input_check(RF_section_parameters_list[i].harmonic_numbers_list[j], n_turns)
-                voltage_array = input_check(RF_section_parameters_list[i].voltage_program_list[j], n_turns)
-                phase_offset_array = input_check(RF_section_parameters_list[i].phi_offset_list[j], n_turns)
-                rf_input_section_arr.append([harmonic_number_array, voltage_array, phase_offset_array])
-            
-            self.rf_input_total_arr.append(rf_input_section_arr)
-            
-            rf_input_section_arr = []
-            
+        temp_voltage_program_list = []
+        temp_voltage_program_list.append(new_RF_section_parameters.voltage_program_list)
+        temp_voltage_program_list.append(Added_RF_section_parameters.voltage_program_list)
+        new_RF_section_parameters.voltage_program_list = temp_voltage_program_list
 
         
-        
+        temp_phi_offset_list = []
+        temp_phi_offset_list.append(new_RF_section_parameters.phi_offset_list)
+        temp_phi_offset_list.append(Added_RF_section_parameters.phi_offset_list)
+        new_RF_section_parameters.phi_offset_list = temp_phi_offset_list
+         
+        return new_RF_section_parameters
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+  
