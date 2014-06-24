d58870e17b7576087b62261143128e629a8feffe
Alexandre Lasheen <alexandre.lasheen@cern.ch>
2014-06-24 18:12:28 +0200

    Added latex equations to the documentations, and few corrections on the slippage factor generation

--- input_parameters/simulation_parameters.py
+++ input_parameters/simulation_parameters.py
@@ -21,10 +21,10 @@ class GeneralParameters(object):
         #: *Particle type*
         self.particle_type = particle_type
         
-        #: *Particle mass in [kg]*
+        #: *Particle mass in [kg]* :math:`: \quad m`
         self.mass = 0
         
-        #: *Particle charge in [C]*
+        #: *Particle charge in [C]* :math:`: \quad q`
         self.charge = 0
         
         # Attribution of mass and charge with respect to particle_type
@@ -34,45 +34,56 @@ class GeneralParameters(object):
         else:
             raise RuntimeError('Particle type not recognized')
         
-        #: | *Counter to be incremented every turn in the tracking loop.*
+        #: | *Counter to be incremented every turn in the tracking loop.* :math:`: \quad n`
         #: | *It is defined as a list in order to be passed as a reference in the other modules.*
         self.counter = [0]
         
         #: *Number of turns of the simulation*
         self.n_turns = n_turns 
         
-        #: *Ring circumference in [m]*
+        #: *Ring circumference in [m]* :math:`: \quad C`
         self.ring_circumference = ring_circumference 
         
-        #: *Ring radius in [m]*
+        #: *Ring radius in [m]* :math:`: \quad R`
         self.ring_radius = self.ring_circumference / (2*np.pi) 
         
-        #: | *Momentum program in [eV/c]*
+        #: | *Momentum (program) in [eV/c]* :math:`: \quad p_n`
         #: | *Can be given as a single value to be assumed constant, or as a program of (n_turns + 1) terms in case of acceleration.*
         self.momentum_program = momentum_program 
         
-        #: *Momentum compation factor (up to 2nd order)*
+        #: *Momentum compation factor (up to 2nd order)* :math:`: \quad \alpha_i`
         self.alpha_array = np.array(alpha_array) 
         
-        #: *Relativistic beta program*
+        #: *Relativistic beta (program)* :math:`: \quad \beta_n`
+        #:
+        #: .. math:: \beta = \sqrt{ 1 + \frac{1}{1 + \left(\frac{mc^2}{ep}\right)^2} }
         self.beta_rel_program = np.sqrt(1 / (1 + (self.mass * c**2)**2 / 
-                                             (self.momentum_program * e)**2)) 
+                                             (self.momentum_program * e)**2))
+
         
-        #: *Relativistic gamma program*
+        #: *Relativistic gamma (program)* :math:`: \quad \gamma_n`
+        #:
+        #: .. math:: \gamma = \sqrt{ 1 + \left(\frac{ep}{mc^2}\right)^2 }
         self.gamma_rel_program = np.sqrt(1 + (self.momentum_program * e)**2 / 
                                          (self.mass * c**2)**2) 
         
-        #: *Energy program in [eV]*
+        #: *Energy (program) in [eV]* :math:`: \quad E_n`
         self.energy_program = np.sqrt(self.momentum_program**2 + 
                                       (self.mass * c**2 / e)**2) 
         
-        #: *Slippage factor (order 0)*
+        #: *Slippage factor (order 0)* :math:`: \quad \eta_{0,n}`
+        #:
+        #: .. math:: \eta_0 = \alpha_0 - \frac{1}{\gamma^2}
         self.eta0 = 0
         
-        #: *Slippage factor (order 1)*
+        #: *Slippage factor (order 1)* :math:`: \quad \eta_{1,n}`
+        #:
+        #: .. math:: \eta_1 = \frac{3\beta^2}{2\gamma^2} + \alpha_1 - \alpha_0\eta_0
         self.eta1 = 0
         
-        #: *Slippage factor (order 2)*
+        #: *Slippage factor (order 2)* :math:`: \quad \eta_{2,n}`
+        #:
+        #: .. math:: \eta_2 = -\frac{\beta^2\left(5\beta^2-1\right)}{2\gamma^2} + \alpha_2 - 2\alpha_0\alpha_1 + \frac{\alpha_1}{\gamma^2} + \alpha_0^2\eta_0 - \frac{3\beta_0^2\alpha_0}{2\gamma_0^2}
         self.eta2 = 0
         
         # Warning that higher orders for alpha_array will not be used
@@ -96,9 +107,8 @@ class GeneralParameters(object):
                 
     def eta_generation(self):
         '''
-        *Pre-processing of the slippage factor parameters with respect to the
-        input momentum compaction factor (up to 2nd order) and the momentum program.
-        For eta coefficients, see Lee: Accelerator Physics (Wiley).*
+        | *Pre-processing of the slippage factor parameters with respect to the input momentum compaction factor (up to 2nd order) and the momentum program.*
+        | *For eta coefficients, see Lee: Accelerator Physics (Wiley).*
         '''
         
         for i in xrange(self.alpha_array.size):
@@ -127,7 +137,7 @@ class GeneralParameters(object):
                 
         self.eta1 = 3 * self.beta_rel_program**2 / (2 * self.gamma_rel_program**2) + \
                     self.alpha_array[1] - \
-                    self.alpha_array[0] * (self.alpha_array[0] - self.gamma_rel_program**-2)
+                    self.alpha_array[0] * self.eta0
         return self.eta1
     
     
@@ -140,7 +150,7 @@ class GeneralParameters(object):
         self.eta2 = - self.beta_rel_program**2 * (5 * self.beta_rel_program**2 - 1) / (2 * self.gamma_rel_program**2) + \
                     self.alpha_array[2] - 2 * self.alpha_array[0] * self.alpha_array[1] + \
                     self.alpha_array[1] / self.gamma_rel_program**2 + \
-                    self.alpha_array[0]**2 * (self.alpha_array[0] - self.gamma_rel_program**-2) - \
+                    self.alpha_array[0]**2 * self.eta0 - \
                     3 * self.beta_rel_program**2 * self.alpha_array[0] / (2 * self.gamma_rel_program**2)
         return self.eta2
         
