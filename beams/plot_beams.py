bfbdbc3c39739aa0f99143537de02ae2d5e4e5a7
dquartul <dquartul@cern.ch>
2014-08-12 23:45:25 +0200

    cloned the PyLongitudinal branch in order to test the various modifications

--- beams/plot_beams.py
+++ beams/plot_beams.py
@@ -8,14 +8,15 @@
 from __future__ import division
 import h5py
 import os
+import subprocess
+import sys
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
-import subprocess
-from scipy.constants import c, e
+from scipy.constants import c
 from trackers.longitudinal_utilities import separatrix
-import sys
-from impedances.longitudinal_impedance import *
+
+
 
 
 if os.path.exists('fig'):    
@@ -296,172 +297,3 @@ def plot_position_evol(counter, beam, h5file, General_parameters, unit = None, s
 
 
 
-def plot_impedance_vs_frequency(counter, general_params, ind_volt_from_imp, 
-                                option1 = "sum", option2 = "no_spectrum", 
-                                option3 = "freq_fft", style = '-', dirname = 'fig'):
-
-    """
-    Plot of impedance vs frequency.
-    """
-
-    # Directory where longitudinal_plots will be stored
-    fig_folder(dirname)
-    
-    if option1 == "sum":
-        
-        ax1 = plt.subplots()[1]
-        ax1.plot(ind_volt_from_imp.frequency_fft, 
-                 ind_volt_from_imp.impedance_array.real, style)
-        ax1.plot(ind_volt_from_imp.frequency_fft, 
-                 ind_volt_from_imp.impedance_array.imag, style)
-        if option2 == "spectrum":
-            ax2 = ax1.twinx()
-            ax2.plot(ind_volt_from_imp.frequency_fft, 
-                     np.abs(ind_volt_from_imp.spectrum))
-        fign = dirname +'/sum_imp_vs_freq_fft' "%d" %counter + '.png'
-        plt.savefig(fign, dpi=300)
-        plt.clf()
-    
-    elif option1 == "single":
-        
-        fig0 = plt.figure(0)
-        ax0 = fig0.add_subplot(111)
-        fig1 = plt.figure(1)
-        ax1 = fig1.add_subplot(111)
-        for i in range(len(ind_volt_from_imp.impedance_sum)):
-                if isinstance(ind_volt_from_imp.impedance_sum[i], 
-                              Longitudinal_table) and option3 == "freq_table":
-                    ax0.plot(ind_volt_from_imp.impedance_sum[i].frequency_array, 
-                             ind_volt_from_imp.impedance_sum[i].Re_Z_array, style)
-                    ax1.plot(ind_volt_from_imp.impedance_sum[i].frequency_array, 
-                             ind_volt_from_imp.impedance_sum[i].Im_Z_array, style) 
-                else:
-                    ax0.plot(ind_volt_from_imp.frequency_fft, 
-                             ind_volt_from_imp.impedance_sum[i].impedance.real, style)
-                    ax1.plot(ind_volt_from_imp.frequency_fft, 
-                             ind_volt_from_imp.impedance_sum[i].impedance.imag, style)
-        
-        fign1 = dirname +'/real_imp_vs_'+option3+'_' "%d" %counter + '.png'
-        if option2 == "spectrum":
-            ax2 = ax0.twinx()
-            ax2.plot(ind_volt_from_imp.frequency_fft, 
-                     np.abs(ind_volt_from_imp.spectrum))
-        plt.figure(0)
-        plt.savefig(fign1, dpi=300)
-        plt.clf()
-        fign2 = dirname +'/imag_imp_vs_'+option3+'_' "%d" %counter + '.png'
-        plt.figure(1)
-        if option2 == "spectrum":
-            ax3 = ax1.twinx()
-            ax3.plot(ind_volt_from_imp.frequency_fft, 
-                     np.abs(ind_volt_from_imp.spectrum))
-        plt.savefig(fign2, dpi=300)
-        plt.clf()
-        
-   
-   
-def plot_induced_voltage_vs_bins_centers(counter, general_params, 
-                                         ind_volt_from_imp, style = '-', 
-                                         dirname = 'fig'):
-
-    """
-    Plot of induced voltage vs bin centers.
-    """
-
-    # Directory where longitudinal_plots will be stored
-    fig_folder(dirname)
-    
-    plt.plot(ind_volt_from_imp.slices.bins_centers, ind_volt_from_imp.ind_vol, style)
-             
-    # Save plot
-    fign = dirname +'/induced_voltage_' "%d" %counter + '.png'
-    plt.savefig(fign)
-    plt.clf()
-
-
-
-def plot_beam_profile(counter, general_params, slices, style = '-', 
-                      dirname = 'fig'):
-    
-    """
-    Plot of longitudinal beam profile
-    """
- 
-    fig_folder(dirname)
-    plt.plot(slices.bins_centers, slices.n_macroparticles, style)
-    fign = dirname +'/beam_profile_' "%d" %counter + '.png'
-    plt.savefig(fign)
-    plt.clf()
-
-
-
-def plot_beam_profile_derivative(counter, general_params, slices, style = '-', 
-                                 dirname = 'fig', numbers = [3]):
-
-    """
-    Plot of the derivative of the longitudinal beam profile.
-    """
-
-    
-    fig_folder(dirname)
-    if 1 in numbers:
-        x1, derivative1 = slices.beam_profile_derivative(1)
-        plt.plot(x1, derivative1, style)
-    if 2 in numbers:
-        x2, derivative2 = slices.beam_profile_derivative(2)
-        plt.plot(x2, derivative2, style)
-    if 3 in numbers:
-        x3, derivative3 = slices.beam_profile_derivative(3)
-        plt.plot(x3, derivative3, style)
-    fign = dirname +'/beam_profile_derivative_' "%d" %counter + '.png'
-    plt.savefig(fign)
-    plt.clf()
-         
-    
-
-def plot_noise_spectrum(frequency, spectrum, sampling = 1, dirname = 'fig'):
-    
-    """
-    Plot of the phase noise spectrum.
-    For large amount of data, use "sampling" to plot a fraction of the data.
-    """
-
-    # Directory where longitudinal_plots will be stored
-    fig_folder(dirname)
-    
-    # Plot
-    plt.figure(1, figsize=(8,6))
-    ax = plt.axes([0.12, 0.1, 0.82, 0.8])
-    ax.plot(frequency[::sampling], spectrum[::sampling])
-    ax.set_xlabel("Frequency [Hz]")
-    params = {'text.usetex': False, 'mathtext.default' : 'sf'}
-    plt.rcParams.update(params)
-    ax.set_ylabel (r"Noise spectrum [$\frac{rad^2}{Hz}$]")
-    fign = dirname +'/noise_spectrum.png'
-    plt.savefig(fign)
-    plt.clf()
-    
-    
-    
-def plot_phase_noise(time, dphi, sampling = 1, dirname = 'fig'):
-    
-    """
-    Plot of the phase noise as a function of time.
-    For large amount of data, use "sampling" to plot a fraction of the data.
-    """
-
-    # Directory where longitudinal_plots will be stored
-    fig_folder(dirname)
-    
-    # Plot
-    plt.figure(1, figsize=(8,6))
-    ax = plt.axes([0.12, 0.1, 0.82, 0.8])
-    ax.plot(time[::sampling], dphi[::sampling])
-    ax.set_xlabel("Time [s]")
-    ax.set_ylabel (r"Phase noise [rad]")
-    fign = dirname +'/phase_noise.png'
-    plt.savefig(fign)
-    plt.clf()     
-    
-    
-       
